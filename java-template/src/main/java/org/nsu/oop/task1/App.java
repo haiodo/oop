17e4a1dde5c4b01adac64f8b0f1a7f5986e74b31
Andrey Sobolev <haiodo@gmail.com>
2024-02-05 00:26:34 +0700

    OOP Java

---
+++ java-template/src/main/java/org/nsu/oop/task1/App.java
@@ -0,0 +1,13 @@
+package org.nsu.oop.task1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
